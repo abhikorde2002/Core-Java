796a77150f59b6bc91c7767ab44cc4400e21b081
Abhishek Korde <abhishekkorde44@gmail.com>
2022-11-15 17:09:54 +0530

    Math class Added

---
+++ src/classcomponent/MathClass.java
@@ -0,0 +1,14 @@
+package classcomponent;
+
+import java.net.Socket;
+
+public class MathClass {
+    public static void main(String[] args) {
+        System.out.println("Max Value is :- "+Math.max(10,20));
+        System.out.println("Min value is:- " +Math.min(2, 1));
+        System.out.println((int )Math.pow(2, 3));
+        System.out.println((int )Math.sqrt(6));
+        System.out.println(Math.random());
+
+    }
+}
