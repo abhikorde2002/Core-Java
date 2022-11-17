fe4be00913671adc0aefee4101507d936d2f5558
Abhishek Korde <abhishekkorde44@gmail.com>
2022-11-17 18:52:19 +0530

    Rhombus Pattern

---
+++ src/Pattern/Hollow_pattern/solid_rhombus.java
@@ -0,0 +1,28 @@
+package Pattern.Hollow_pattern;
+
+class solid_rhombus{
+    public static void main(String[] args) {
+        int num=5;
+        rhombus(num);
+    }
+
+    static void rhombus(int num){
+        for(int i=1;i<=num;i++){
+            for(int space=1;space<=num-i;space++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=num;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    /*
+    *****
+   *****
+  *****
+ *****
+*****
+     */
+
+}
