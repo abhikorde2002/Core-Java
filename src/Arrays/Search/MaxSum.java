674b02cbb2db7afdeff0e29dd805df587b464800
Abhishek korde <abhishekkorde44@gmail.com>
2022-11-22 16:33:33 +0530

    Cod Added

--- src/Arrays/Search/MaxSum.java
+++ src/Arrays/Search/MaxSum.java
@@ -2,7 +2,7 @@ package Search;
 
 public class MaxSum {
     public static void main(String[] args) {
-        int arr[] = { 2, 4, 6, 8, 10 };
+        int arr[] = { 1, -2, 6, -1, 3 };
         maxSubarray(arr);
     }
 
---
+++ src/Arrays/Search/PrefixofSubArray.java
@@ -0,0 +1,32 @@
+package Search;
+
+public class PrefixofSubArray {
+    public static void main(String[] args) {
+        int arr[] = { 1, -2, 6, -1, 3 };
+        Prefix(arr);
+    }
+
+    static void Prefix(int arr[]) {
+        int currentSum = 0;
+        int MaxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+
+        prefix[0] = arr[0];
+        // Calculate Prifix
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                currentSum = start == 0 ? prefix[end] : prefix[end] + prefix[start - 1];
+                if (MaxSum < currentSum) {
+                    MaxSum = currentSum;
+                }
+            }
+        }
+        System.out.println("Max sum is :- " + MaxSum);
+    }
+}
