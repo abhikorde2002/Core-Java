4e036a536c7421e46087e466430c59b3941a5fd4
Abhishek korde <abhishekkorde44@gmail.com>
2022-12-02 20:31:08 +0530

    Clear last ith Bit in Range

---
+++ src/Bit_Manipulation/ClearIBits.java
@@ -0,0 +1,14 @@
+package Bit_Manipulation;
+
+public class ClearIBits {
+    public static void main(String[] args) {
+        System.out.println(clearI(10, 2, 4));
+    }
+
+    static int clearI(int n, int i, int j) {
+        int a = ((~0) << (j + 1));
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+}
