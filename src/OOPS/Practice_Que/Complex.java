abfd39f37b9e01dd4ba004453b93db951de7fd50
Abhishek korde <abhishekkorde44@gmail.com>
2022-12-07 10:03:58 +0530

    Practice Q1

---
+++ src/OOPS/Practice_Que/Complex.java
@@ -0,0 +1,50 @@
+package OOPS.Practice_Que;
+
+/*
+ Question1:Print the sum, difference and product of two complex numbers by creating a class named 'Complex' with separate 
+ methods for each operation who sereal and imaginary parts are entered by the user.
+ */
+class Complex {
+    int real;
+    int imag;
+
+    public Complex(int r, int i) {
+        real = r;
+        imag = 1;
+    }
+
+    public static Complex add(Complex a, Complex b) {
+        return new Complex((a.real + b.real), (a.imag + b.imag));
+    }
+
+    public static Complex diff(Complex a, Complex b) {
+        return new Complex((a.real - b.real), (a.imag - b.imag));
+    }
+
+    public static Complex product(Complex a, Complex b) {
+        return new Complex(((a.real * b.real) - (a.imag * b.imag)), ((a.real * b.imag) + (a.imag * b.real)));
+    }
+
+    public void printComplex() {
+        if (real == 0 && imag != 0) {
+            System.out.println(imag + "i");
+        } else if (imag == 0 && real != 0) {
+            System.out.println(real);
+        } else {
+            System.out.println(real + "+" + imag + "i");
+        }
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Complex c = new Complex(4, 5);
+        Complex d = new Complex(9, 4);
+        Complex e = Complex.add(c, d);
+        Complex f = Complex.diff(c, d);
+        Complex g = Complex.product(c, d);
+        e.printComplex();
+        f.printComplex();
+        g.printComplex();
+    }
+}
