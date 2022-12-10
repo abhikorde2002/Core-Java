b981d5957930b1741d04938edf6ec17e11c95557
Abhishek korde <abhishekkorde44@gmail.com>
2022-12-10 13:37:49 +0530

    Seprating Separate the Individual Characters from a Strin

---
+++ src/String/Seprating_char.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Seprating_char {
+    public static void main(String[] args) {
+        String str = "Pentagon";
+        char ch;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+
+            System.out.print(ch + " ");
+        }
+
+    }
+}
