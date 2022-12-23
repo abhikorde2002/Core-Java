4fc66c2b0474ac073147fa0e7bbb9750531c3d5a
Abhishek korde <abhishekkorde44@gmail.com>
2022-12-23 17:59:48 +0530

    N Queen Problem

--- src/Backtracking/N_Que_1Sol.java
+++ src/Backtracking/N_Que_1Sol.java
@@ -1,8 +1,11 @@
 package Backtracking;
 
-public class N_Queens {
+import javax.xml.transform.SourceLocator;
+
+// Chek if proble can be solve & print only 1 solution to N Queens problem
+public class N_Que_1Sol {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 6;
         char board[][] = new char[n][n];
 
         for (int i = 0; i < n; i++) {
@@ -10,54 +13,58 @@ public class N_Queens {
                 board[i][j] = 'x';
             }
         }
-        nQueen(board, 0);
-        System.out.println("Total ways of solving n queens problem is :- " + count);
+        if (nQueen(board, 0)) {
+            System.out.println("Solution is possible:- ");
+            printBoard(board);
+        } else {
+            System.out.println("Solution is not posible");
+        }
     }
 
-    static boolean isSafe(char board[][], int row, int col) {
-        // Vertical up
+    static boolean isSafe(char boar[][], int row, int col) {
+        // vertical up
         for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q') {
+            if (boar[i][col] == 'Q') {
                 return false;
             }
         }
         // diag left up
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
+            if (boar[i][j] == 'Q') {
                 return false;
             }
         }
         // diag right up
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < boar.length; i--, j++) {
+            if (boar[i][j] == 'Q') {
                 return false;
             }
         }
         return true;
-
     }
 
-    static int count = 0;
-
-    static void nQueen(char board[][], int row) {
+    static boolean nQueen(char board[][], int row) {
         // base
         if (row == board.length) {
-            printBoard(board);
-            count++;
-            return;
+            // printBoard(board);
+            return true;
         }
         // colum loop
         for (int j = 0; j < board.length; j++) {
             if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';
-                nQueen(board, row + 1);// Function call
-                board[row][j] = 'x';// backtrackig step
+                if (nQueen(board, row + 1)) {
+                    return true;
+                }
+
+                board[row][j] = 'X';
             }
         }
+        return false;
     }
 
     static void printBoard(char board[][]) {
-        System.out.println("------Chess Board ------");
+        System.out.println("---- Chess Board ----");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j] + " ");
