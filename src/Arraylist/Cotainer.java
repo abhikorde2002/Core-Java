31e10ff838980d90c68487e53149b10f5e39d566
Abhishek korde <abhishekkorde44@gmail.com>
2023-01-01 15:52:32 +0530

    Store Water

--- src/Arraylist/Cotainer.java
+++ src/Arraylist/Cotainer.java
@@ -15,21 +15,44 @@ public class Cotainer {
         hight.add(8);
         hight.add(3);
         hight.add(7);
-        System.out.println(storeWater(hight));
+        storeWater(hight);
 
     }
 
-    static int storeWater(ArrayList<Integer> hight) {
-        int maxWater = 0;
+    // static int storeWater(ArrayList<Integer> hight) {
+    // int maxWater = 0;
+    // //Brute Force
+    // for (int i = 0; i < hight.size(); i++) {
+    // for (int j = i + 1; j < hight.size(); j++) {
+    // int ht = Math.min(hight.get(i), hight.get(j));
+    // int width = j - i;
+    // int currWater = ht * width;
+    // maxWater = Math.max(maxWater, currWater);
+    // }
+    // }
+    // return maxWater;
+    // }
 
-        for (int i = 0; i < hight.size(); i++) {
-            for (int j = i + 1; j < hight.size(); j++) {
-                int ht = Math.min(hight.get(i), hight.get(j));
-                int width = j - i;
-                int currWater = ht * width;
-                maxWater = Math.max(maxWater, currWater);
+    static void storeWater(ArrayList<Integer> height) {
+        // Calculate water area
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+
+        while (lp < rp) {
+            // Calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+
+            // Update ptr
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
             }
         }
-        return maxWater;
+        System.out.println(maxWater);
     }
 }
