6a33d08a4bc0069c16b9754721898facfae1a668
Abhishek korde <abhishekkorde44@gmail.comm>
2023-01-22 10:32:47 +0530

    hello

---
+++ src/OOPS/Interface/Return.java
@@ -0,0 +1,14 @@
+package OOPS.Interface;
+
+// public class Return {
+//     void display(){
+//         System.out.println("Hello , coder");
+//     }
+// }
+// class child extends Return{
+//     int display(){
+//         int c= 10+20;
+//         return c;
+//     }
+// }
+//Return type should be same same
