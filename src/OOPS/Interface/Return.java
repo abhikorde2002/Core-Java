85a6c3fced438ff16fafe02d4979402d64ba7bba
Abhishek korde <abhishekkorde44@gmail.comm>
2023-01-23 15:00:40 +0530

    first commit

--- src/OOPS/Interface/Return.java
+++ src/OOPS/Interface/Return.java
@@ -1,14 +1,17 @@
 package OOPS.Interface;
 
-// public class Return {
-//     void display(){
-//         System.out.println("Hello , coder");
-//     }
-// }
-// class child extends Return{
-//     int display(){
-//         int c= 10+20;
-//         return c;
-//     }
-// }
-//Return type should be same same
+
+class Program1 {
+    public static void main(String args[]) {
+    String s1 = "Rahul";
+    String s2 = "Rahul";
+    String s3 = new String("rahul");
+    String s4 = new String("Raju");
+    System.out.print(s1==s3);
+    System.out.print(s1==s2);
+    System.out.print(s1.equalsIgnoreCase(s3));
+    }
+    }
+
+
+// Return type should be same same
