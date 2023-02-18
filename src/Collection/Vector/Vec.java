098f43125ca42a2d99c6607f077472c1b08362f4
Abhishek korde <abhishekkorde44@gmail.comm>
2023-02-18 18:43:09 +0530

    Vector

---
+++ src/Collection/Vector/Vec.java
@@ -0,0 +1,15 @@
+package Collection.Vector;
+
+import java.util.Vector;
+
+public class Vec {
+    public static void main(String[] args) {
+        Vector books = new Vector<>();
+        books.add("Java");
+        books.add("Phython");
+        books.add("R");
+        books.add("C");
+        books.add("C#");
+        System.out.println(books);
+    }
+}
