19f2ccff518c0cd61a76299a9e298e2f9cd5b684
Abhishek korde <abhishekkorde44@gmail.com>
2023-03-21 23:41:54 +0530

    Series Quesion 3

--- src/Practice_Quesion/Series_Program/Third_Que.java
+++ src/Practice_Quesion/Series_Program/Third_Que.java
@@ -7,16 +7,22 @@ public class Third_Que {
         Scanner sc= new Scanner(System.in);
         int x=sc.nextInt();
         int n=sc.nextInt();
+        thirde(x, n);
+    }
+
+    static void thirde(int x,int n){
+        int fact=1;
         int sum=1;
-        for(int i=1;i<=n;i++){
-          int num=(int) Math.pow((1+x), i);
-          int fact=1;
-        for(int j=2;j<=(n+1);j++){
-            fact=fact*j;
-        }  
-         
-         sum= num/fact;
+        int num=0;
+        for(int i=2;i<=(n+1);i++){
+            fact=fact*i;
+         for(int j=1;j<=n;j++){
+             num=(int) Math.pow((1+x),j)/fact;
+          
+         }
+         System.out.println("So series is :- "+num);
+         System.out.println("Fact value is:-  " + fact);
+
         }
-        System.out.println(sum);
     }
 }
