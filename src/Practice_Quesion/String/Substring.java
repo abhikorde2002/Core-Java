c751641ce4e368d7206d7d84b9ae7814885b754c
Abhishek korde <abhishekkorde44@gmail.com>
2023-03-22 00:31:14 +0530

    Substring

---
+++ src/Practice_Quesion/String/Substring.java
@@ -0,0 +1,22 @@
+package Practice_Quesion.String;
+
+public class Substring {
+    public static void main(String[] args) {
+        SubString("Abhishek");
+    }
+
+    static void SubString(String str){
+        char ch[]=str.toCharArray();
+
+        for(int i=0;i<str.length();i++){
+            int start=i;
+            for(int j=i;j<str.length();j++){
+                int end=j;
+                for(int k=start;k<=end;k++){
+                    System.out.print(ch[k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
