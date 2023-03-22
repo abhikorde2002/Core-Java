2a473c896ae1ac0364b5cf0797b6ec964f4d6fe9
Abhishek korde <abhishekkorde44@gmail.com>
2023-03-22 12:08:36 +0530

    Marge array

---
+++ src/Practice_Quesion/Array/Concate.java
@@ -0,0 +1,23 @@
+package Practice_Quesion.Array;
+
+import java.util.Arrays;
+
+class Concate{ 
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40};
+        int num[]={50,60,70,80};
+        concat(arr, num);
+    }
+
+    static void concat(int arr[],int num[]){
+       int ans[]=new int[2*arr.length];
+       for(int i=0;i<arr.length;i++){
+        ans[i]=arr[i];
+        ans[i+arr.length]=num[i];
+        
+       }
+      
+       System.out.println(Arrays.toString(ans));
+    }
+
+ }
